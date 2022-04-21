78e453ca5d9d15332ef400d6ccd65dea0f8326a1
Ashu Kumar <65837956+ashuspy@users.noreply.github.com>
2022-04-21 11:03:18 +0530

    Create SmallFactorial.java

---
+++ src/Arrays/SmallFactorial.java
@@ -0,0 +1,39 @@
+import java.util.*;  
+
+import java.math.BigInteger;
+
+public class TestClass{
+
+    static BigInteger factorial(int N){
+
+        BigInteger f
+
+            = new BigInteger("1");
+
+        for (int i = 2; i <= N; i++)
+
+            f = f.multiply(BigInteger.valueOf(i));
+
+        return f;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int i = 0; i<T; i++){
+
+            int ele = sc.nextInt();
+
+            System.out.println(factorial(ele));
+
+        }
+
+        sc.close();
+
+    }
+
+}
