f5bf440ffb6eabd4c00de0886c52b49b7ffca5e2
Ashu Kumar <ashuk1545@gmail.com>
2022-07-20 15:36:04 +0530

    initial commit

---
+++ src/SDE_SHeet/Average.java
@@ -0,0 +1,15 @@
+package SDE_SHeet;
+
+public class Average {
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = {1,2,3,4,5};
+        double sum = 0;
+        for (int i =0; i<n;i++){
+            sum += (double)arr[i];
+        }
+        double average = sum/n;
+        System.out.println("the averge is  "+average);
+
+    }
+}
